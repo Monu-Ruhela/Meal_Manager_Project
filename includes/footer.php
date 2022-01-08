efbbe17a5933797bcbdc97d1f9d61384e0951ebc
monuruhela21 <66027443+Monu-Ruhela@users.noreply.github.com>
2022-01-08 23:21:00 +0530

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,7 @@
+	<div class="footer">
+		<div class="container">
+			<p class="wow fadeInLeft" data-wow-delay="0.4s">&copy; Meal Manager &nbsp;</p><br>
+			<p class="wow fadeInLeft" data-wow-delay="0.4s">&copy; KIET Group Of Institutions &nbsp;</p><br>
+
+		</div>
+	</div>
